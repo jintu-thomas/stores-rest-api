a56638db8f8c4d8b6baad5f19961b6c8aff782f0
jintuTP <jintu.thomas@varadhismartek.in>
2021-07-22 22:41:33 +0530

    fix

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 
 from flask import Flask
 from flask_restful import Api
@@ -12,7 +11,7 @@ from resources.stores import Store,StoreList
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
