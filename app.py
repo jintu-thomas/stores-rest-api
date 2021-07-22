2863cdbf4ccc31563c745ddfeb625739440ecf81
jintuTP <jintu.thomas@varadhismartek.in>
2021-07-22 13:10:44 +0530

    first commit, add rest api to access items,and user authentication and store

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from resources.item import Item,ItemList
+from security import authentcate,identity 
+from resources.user import UserRegister
+from resources.stores import Store,StoreList
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'jose'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app,authentcate,identity)         #jwt create new endpoint calle s /auth`
+
+
+
+api.add_resource(Store,'/store/<string:name>')
+api.add_resource(StoreList,'/stores')
+api.add_resource(ItemList,'/items')
+api.add_resource(Item,'/item/<string:name>')          #https://127.0.0.1:5000/sttudent/jintu
+api.add_resource(UserRegister, '/register')
+
+if __name__=='__main__':
+    from db import db
+    db.init_app(app)        #circular imports
+    app.run(port = 5000,debug=True)
