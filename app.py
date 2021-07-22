5e8d98315324ef12e619bbf6d7a713192fc19e6a
jintuTP <jintu.thomas@varadhismartek.in>
2021-07-22 17:00:03 +0530

    Addedd posgress envirnment variable and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +10,7 @@ from resources.stores import Store,StoreList
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db') #when the first args is not found, it select second arhs(defaiult)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
