9a679b4e6bd9cbaa6da408db2bd733ad16fc272f
jintuTP <jintu.thomas@varadhismartek.in>
2021-07-23 09:19:26 +0530

    fix postgress

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from resources.stores import Store,StoreList
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgres://ynyesrayzkhjog:77418e3b58fa64f4b1fe892982f0dd4e3048472ac29ca0a8f120f3f3a419a309@ec2-54-73-68-39.eu-west-1.compute.amazonaws.com:5432/d4383tu71jljj','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
