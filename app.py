09d63fb3483d1368433020feba119a8a6a44b18b
jintuTP <jintu.thomas@varadhismartek.in>
2021-07-24 15:42:33 +0530

    postgress

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from resources.stores import Store,StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
